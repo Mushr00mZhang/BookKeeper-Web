b6ff2dc9321cd41b2f4207a0d12bd5bdfea7ba76
Mushr00mZhang <392388987@qq.com>
2024-02-29 23:28:04 +0800

    添加sqlite连接方式

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite" // Sqlite driver based on CGO
+	// "github.com/glebarez/sqlite" // Pure go SQLite driver, checkout https://github.com/glebarez/sqlite for details
+	"gorm.io/gorm"
+)
+
 func main() {
 
+	// github.com/mattn/go-sqlite3
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(1)
+	}
+	fmt.Println(db.Config)
 }
