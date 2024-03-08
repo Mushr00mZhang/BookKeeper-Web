f7bb655ead5000d5c0d213d637833718d6df4373
Mushr00mZhang <392388987@qq.com>
2024-03-09 00:20:50 +0800

    添加测试接口代理

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       '@': '/src/',
     },
   },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+      // '/api': {
+      //   target: 'http://localhost:8080',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+    },
+  },
 });
