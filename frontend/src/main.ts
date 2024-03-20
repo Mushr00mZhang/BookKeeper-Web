99120b6e899c46116b243c5d593937862c88c557
Mushr00mZhang <392388987@qq.com>
2024-03-21 00:15:05 +0800

    【前端】添加基础样式

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue';
-import './style.css';
+import '@/styles/common.scss';
 import App from './App.vue';
 import 'element-plus/es/components/message/style/css';
 import 'element-plus/es/components/tooltip/style/css';
 import { router } from './router';
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
