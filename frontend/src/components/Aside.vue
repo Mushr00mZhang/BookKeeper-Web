22e02ad28e6c043eb10e425bd13a3383b43eb0b6
Mushr00mZhang <392388987@qq.com>
2024-03-23 21:59:17 +0800

    【边栏】调整样式

--- frontend/src/components/Aside.vue
+++ frontend/src/components/Aside.vue
@@ -1,7 +1,16 @@
 <template>
-  <RouterLink to="/">首页</RouterLink>
-  <RouterLink to="/outlaycat/list">支出类型列表</RouterLink>
+  <section class="aside-links">
+    <RouterLink to="/">首页</RouterLink>
+    <RouterLink to="/outlay/list">支出列表</RouterLink>
+    <RouterLink to="/outlaycat/list">支出类型列表</RouterLink>
+  </section>
 </template>
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
 </script>
+<style lang="scss" scoped>
+.aside-links {
+  display: flex;
+  flex-direction: column;
+}
+</style>
