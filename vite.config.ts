07a63c1c325809fda8052d1735b2f1761b486ed1
Mushr00mZhang <392388987@qq.com>
2024-05-24 22:46:24 +0800

    更新分包机制

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ElementPlus from 'unplugin-element-plus/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
+const chunkPackages = [
+  'axios',
+  'echarts',
+  'element-plus',
+  '@element-plus',
+  '@material/web',
+  'lodash-es',
+  'vue',
+  '@vue',
+  'vue-router',
+  'zrender',
+];
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
@@ -14,6 +27,11 @@ export default defineConfig({
         },
       },
     }),
+    // visualizer({
+    //   open: true, // 注意这里要设置为true，否则无效
+    //   gzipSize: true, // 分析图生成的文件名
+    //   brotliSize: true, // 收集 brotli 大小并将其显示
+    // }),
   ],
   resolve: {
     alias: {
@@ -34,13 +52,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          echarts: ['echarts'],
-          'element-plus': ['element-plus'],
-          'material-web': ['@material/web'],
-          vue: ['vue']
-        }
-      }
-    }
-  }
+        manualChunks(id, meta) {
+          const info = meta.getModuleInfo(id);
+          if (!info.isIncluded) return;
+          const pkg = chunkPackages.find((i) => new RegExp(`/${i}/`).test(id));
+          if (pkg) return pkg.replace(/^[@/]+|[@/]+$/g, '').replace(/[@/]+/g, '-');
+          return;
+        },
+      },
+    },
+  },
 });
